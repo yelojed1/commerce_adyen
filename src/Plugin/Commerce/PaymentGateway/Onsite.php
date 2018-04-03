ec60d95d924b9b78a7926a690a746bcb40180fd9
jdenasdetourris <jdetourris@liberal.co.nz>
2018-04-03 04:53:56 +0200

    Update payment gateway config onsite form

--- src/Plugin/Commerce/PaymentGateway/Onsite.php
+++ src/Plugin/Commerce/PaymentGateway/Onsite.php
@@ -99,6 +99,101 @@ class Onsite extends OnsitePaymentGatewayBase implements OnsiteInterface {
       '#required' => TRUE,
     ];
 
+    $form['shopper_locale'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Shopper locale'),
+      '#default_value' => $this->configuration['shopper_locale'],
+      // @link https://docs.adyen.com/developers/hpp-manual#createaskin                                                            
+      '#options' => array_map('t', [
+        'zh' => 'Chinese – Traditional',
+        'cz' => 'Czech',
+        'da' => 'Danish',
+        'nl' => 'Dutch',
+        'en_GB' => 'English – British',
+        'en_CA' => 'English – Canadian',
+        'en_US' => 'English – US',
+        'fi' => 'Finnish',
+        'fr' => 'French',
+        'fr_BE' => 'French – Belgian',
+        'fr_CA' => 'French – Canadian',
+        'fr_CH' => 'French – Swiss',
+        'fy_NL' => 'Frisian',
+        'de' => 'German',
+        'el' => 'Greek',
+        'hu' => 'Hungarian',
+        'it' => 'Italian',
+        'li' => 'Lithuanian',
+        'no' => 'Norwegian',
+        'pl' => 'Polish',
+        'pt' => 'Portuguese',
+        'ru' => 'Russian',
+        'sk' => 'Slovak',
+        'es' => 'Spanish',
+        'sv' => 'Swedish',
+        'th' => 'Thai',
+        'tr' => 'Turkish',
+        'uk' => 'Ukrainian',
+      ]),
+      '#required' => TRUE,
+    ];
+    
+    $form['recurring'] = [
+      '#type' => 'select',
+      '#title' => t('Recurring contract'),
+      '#empty_option' => t('Do not used'),
+      '#options' => [
+        Contract::ONECLICK => t('One click'),
+        Contract::RECURRING => t('Recurring'),
+        Contract::ONECLICK_RECURRING => t('One click, recurring'),
+      ],
+    ];
+
+    $form['state'] = [
+      '#type' => 'select',
+      '#title' => t('Fields state'),
+      '#default_value' => 0,
+      '#description' => t('State of fields on Adyen HPP.'),
+      '#options' => [
+        t('Fields are visible and modifiable'),
+        t('Fields are visible but unmodifiable'),
+        t('Fields are not visible and unmodifiable'),
+      ],
+    ];
+
+    $form['payment_types'] = [
+      '#type' => 'vertical_tabs',
+    ];
+
+    foreach ($payment_types as $payment_type => $data) {
+      $config_form = commerce_adyen_invoke_controller('payment', $payment_type, $settings, $payment_types)->configForm();
+
+      if (!empty($config_form)) {
+        $config_form['#type'] = 'fieldset';
+        $config_form['#title'] = $data['label'];
+
+        $form['payment_types'][$payment_type] = $config_form;
+      }
+
+      // Form a list of payment types and their labels.                                                                            
+      $types[$payment_type] = $data['label'];
+    }
+
+    $form['default_payment_type'] = [
+      '#type' => 'select',
+      '#title' => t('Default payment type'),
+      '#options' => $types,
+      '#disabled' => empty($types),
+      '#description' => t('Selected payment type will be set as default extender for the payment request. This value can be changed during checkout process.'),
+      '#empty_option' => t('- None -'),
+    ];
+
+    $form['use_checkout_form'] = [
+      '#type' => 'checkbox',
+      '#title' => t('Use checkout forms'),
+      '#disabled' => empty($payment_types),
+      '#description' => t('Allow to use checkout forms for filing additional data for the payment type.'),
+    ];
+
     return $form;
   }
 
